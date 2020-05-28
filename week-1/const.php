6a9017f32b9230022e3d615bc79751ea52ff29b7
usman <usman.kamailaev@yandex.ru>
2020-05-28 22:32:13 +0300

    task-0

---
+++ week-1/const.php
@@ -0,0 +1,8 @@
+<?php
+
+const TEST_CONSTANTA = 'asd';
+
+function show_something()
+{
+    return TEST_CONSTANTA;
+}
---
+++ week-1/homework-1.php
@@ -0,0 +1,9 @@
+<?php
+
+require('const.php');
+$user_name = "Igor";
+if (1) {
+    echo 'hi';
+}
+
+show_something();
