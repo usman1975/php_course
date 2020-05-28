4e13848106f3f12611b204810ee813b6767b7da6
usman <usman.kamailaev@yandex.ru>
2020-05-29 00:02:28 +0300

    homework-1

--- week-1/homework-1.php
+++ week-1/homework-1.php
@@ -3,7 +3,15 @@
 require('const.php');
 $user_name = "Igor";
 if (1) {
-    echo 'hi';
+    echo 'hi<br>';
 }
 
-show_something();
+ show_something();
+
+$name = "Усман";
+$age = "45";
+
+echo "Меня зовут:" . " " . $name . "." . "<br>";
+echo "Мне 45 лет" . "." . "<br>";
+echo '"!|/\'"\\' . "<br>";
+echo "\"!|/'\"\\";
