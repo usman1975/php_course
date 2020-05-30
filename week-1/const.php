ef4b4e4518347dc6248eca0a591562e0a704f680
usman <usman.kamailaev@yandex.ru>
2020-05-30 15:56:41 +0300

    JOB2

--- week-1/const.php
+++ week-1/const.php
@@ -6,3 +6,10 @@ function show_something()
 {
     return TEST_CONSTANTA;
 }
+
+//Задание #2
+
+define("ALL_FIGURES", 80);
+define("FELT_PEN", 23);
+define("PENSIL", 40);
+define("PEINT", ALL_FIGURES - (FELT_PEN + PENSIL));
